b283630572c5c1d2d4aabf01bc69d42ecbf559c0
TA GIA BAO <git@github.com>
2024-06-22 23:46:09 +0700

    add: todo app with session-based task management

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["fap", "wank", "jizz"]
+# tasks = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "todo/index.html", {
-        "tasks" : tasks
+        "tasks": request.session["tasks"]
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("todo:index"))
+        else:
+            return render(request, "todo/add.html", {
+                "form": form
+            })
+    
+    return render(request, "todo/add.html", {
+        "form": NewTaskForm()
     })
